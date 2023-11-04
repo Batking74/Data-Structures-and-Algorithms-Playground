6edf4beeddfec10aa7b06e50d37c8f8704428f97
Nazir Knuckles <nazirknuckles@gmail.com>
2023-11-04 15:28:07 -0400

    Completed camel-case Algorithm!!!

--- Algorithms/Javascript/03-camel-case/Unsolved/camel-case.js
+++ Algorithms/Javascript/03-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,21 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+// Time Complexity: O(n)
+const camelCase = function(str) {
+    let camelCaseStr = '';
+
+    for(let i = 0; i < str.length; i++) {
+        let letter = str.charAt(i);
+        if (i == 0) {
+            camelCaseStr += letter.toLowerCase();
+        }
+        else if (letter === ' ') {
+            camelCaseStr += str.charAt(i+1).toUpperCase();
+            i++;
+        }
+        else {
+            camelCaseStr += letter;
+        }
+    }
+    return camelCaseStr;
+};

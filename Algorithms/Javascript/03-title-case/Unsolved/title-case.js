1cdb607237ee3da4ef8a048b038f9bddc0e90d3d
Nazir Knuckles <nazirknuckles@gmail.com>
2023-11-04 16:00:20 -0400

    Completed vowel-count Algorithm!! Also, improved the time it takes to complete title-case Algorithm slightly.

--- Algorithms/Javascript/03-title-case/Unsolved/title-case.js
+++ Algorithms/Javascript/03-title-case/Unsolved/title-case.js
@@ -1,14 +1,19 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
+// Time Complexity: O(n)
 var titleCase = function(str) {
     let newString = '';
     for (let i = 0; i < str.length; i++) {
-        let letter = str.substring(i, i + 1);
+        let letter = str.charAt(i);
         if (i == 0) {
             letter = letter.toUpperCase();
         }
         else if (letter === ' ') {
-            letter = (str.substring(i, i + 2)).toUpperCase();
+            // Original Solution: letter = (str.substring(i, i + 2)).toUpperCase();
+
+            // New Solution that takes a little less time
+            letter = ` ${(str.charAt(i+1)).toUpperCase()}`;
+
             i++;
         }
         newString += letter;
--- Algorithms/Javascript/03-vowel-count/Unsolved/vowel-count.js
+++ Algorithms/Javascript/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,16 @@
 // Write code to return the the number of vowels in `str`
+// vowels: 'a', 'e', 'i', 'o', and 'u'
 
-var vowelCount = function(str) {};
+// Time Complexity: O(n)
+var vowelCount = function(str) {
+    let numOfVowels = 0;
+    for(let i = 0; i < str.length; i++) {
+        const letter = str.charAt(i);
+        if(letter === 'A' || letter === 'a') numOfVowels++;
+        else if(letter === 'E' || letter === 'e') numOfVowels++;
+        else if(letter === 'I' || letter === 'i') numOfVowels++;
+        else if(letter === 'O' || letter === 'o') numOfVowels++;
+        else if(letter === 'U' || letter === 'u') numOfVowels++;
+    }
+    return numOfVowels;
+};
